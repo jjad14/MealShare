e7f682818102d89f281d42086712631101f7a946
Dennis <dmarlo96@live.ca>
2025-05-22 16:15:09 -0400

    Nav Link styling fix

--- components/header/nav-link.js
+++ components/header/nav-link.js
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import styles from './header.module.css';
+import styles from './nav-link.module.css';
 
 export default function NavLink({ href, children }) {
 	const path = usePathname();
@@ -10,7 +10,11 @@ export default function NavLink({ href, children }) {
 	return (
 		<Link
 			href={href}
-			className={path.startsWith(href) ? styles.active : undefined}>
+			className={
+				path.startsWith(href)
+					? `${styles.link} ${styles.active}`
+					: `${styles.link}`
+			}>
 			{children}
 		</Link>
 	);
