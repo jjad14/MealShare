40c5f68836e11b4ba0f237ff1c31e8d8daee074d
Dennis <dmarlo96@live.ca>
2025-05-23 14:57:04 -0400

    Image Picker Preview

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,19 +1,49 @@
 'use client';
 
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import styles from './image-picker.module.css';
+import Image from 'next/image';
 
 export default function ImagePicker({ label, name }) {
+	const [pickedImage, setPickedImage] = useState(null);
 	const imageInputRef = useRef();
 
 	const handlePickerClick = () => {
 		imageInputRef.current.click();
 	};
 
+	const handleImageChange = (event) => {
+		const file = event.target.files[0];
+
+		// If no file is selected, do nothing
+		if (!file) {
+			setPickedImage(null);
+			return;
+		}
+
+		const fileReader = new FileReader();
+		// Once the file is loaded, this function is called with the
+		// file's contents as a data URL. This is then used to set the
+		// component's state, which will cause the component to render
+		// the image preview.
+		fileReader.onload = () => {
+			setPickedImage(fileReader.result);
+		};
+
+		// Read the file as a data URL
+		fileReader.readAsDataURL(file);
+	};
+
 	return (
 		<div className={styles.picker}>
 			<label htmlFor={name}>{label}</label>
 			<div className={styles.controls}>
+				<div className={styles.preview}>
+					{!pickedImage && <p>No image picked yet.</p>}
+					{pickedImage && (
+						<Image src={pickedImage} alt='Selected image' fill />
+					)}
+				</div>
 				<input
 					className={styles.input}
 					type='file'
@@ -21,6 +51,7 @@ export default function ImagePicker({ label, name }) {
 					accept='image/png, image/jpeg'
 					name={name}
 					ref={imageInputRef}
+					onChange={handleImageChange}
 				/>
 				<button
 					className={styles.button}
