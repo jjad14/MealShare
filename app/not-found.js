97a0f16f0c2bb80b745eb421e6bb7e8bc76d9c3f
Dennis <dmarlo96@live.ca>
2025-05-23 14:15:04 -0400

    Not Found Page

---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+'use client';
+
+export default function NotFoundPage() {
+	return (
+		<main className='not-found'>
+			<h1>404 - Page Not Found</h1>
+			<p>Could not find requested page orresource</p>
+		</main>
+	);
+}
