2c3e4024d9bd3c975d72341b4d500d4ac6fe8f41
Dennis <dmarlo96@live.ca>
2025-05-23 14:36:22 -0400

    Single Meal Page Component

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,3 +1,38 @@
-export default function MealDetailsPage() {
-	return <h1>Meal Details Page</h1>;
+import Image from 'next/image';
+import styles from './page.module.css';
+import { getMeal } from '@/lib/meals';
+
+export default async function MealDetailsPage({ params }) {
+	const { mealSlug } = await params;
+	const meal = getMeal(mealSlug);
+
+	// Replace newlines with <br> tags
+	meal.instructions = meal.instructions.replace(/\n/g, '<br>');
+
+	return (
+		<>
+			<header className={styles.header}>
+				<div className={styles.image}>
+					<Image src={meal.image} alt={meal.title} fill />
+				</div>
+				<div className={styles.headerText}>
+					<h1>{meal.title}</h1>
+					<p className={styles.creator}>
+						by{' '}
+						<a href={`malito:${meal.creator_email}`}>
+							{meal.creator}
+						</a>
+					</p>
+					<p className={styles.summary}>{meal.summary}</p>
+				</div>
+			</header>
+			<main className={styles.main}>
+				<p
+					className={styles.instructions}
+					dangerouslySetInnerHTML={{
+						__html: meal.instructions
+					}}></p>
+			</main>
+		</>
+	);
 }
